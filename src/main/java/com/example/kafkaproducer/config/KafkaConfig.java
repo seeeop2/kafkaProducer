deb9139e612357276c34e56d13df098a2d5945a7
seeeop2 <seeeop2@gmail.com>
2025-01-22 21:48:41 +0900

    FEAT: Kafka Consumer 설정 추가 및 리스너 컨테이너 팩토리 구성
    - Kafka Consumer를 위한 ConsumerFactory 메소드 추가
      - Kafka 서버 주소, 키 및 값의 역직렬화 설정 포함
    - ConcurrentKafkaListenerContainerFactory 메소드를 추가하여 Kafka 리스너 컨테이너를 구성
      - consumerFactory를 사용하여 설정된 ConsumerFactory를 리스너 컨테이너에 적용
    - 기존의 Producer 설정은 유지하며, 새로운 Consumer 설정을 추가하여 생산자와 소비자 모두 지원

--- src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
+++ src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
@@ -1,11 +1,14 @@
 package com.example.kafkaproducer.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -31,5 +34,22 @@ public class KafkaConfig {
         myConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // 값 직렬화 설정
 
         return new DefaultKafkaProducerFactory<>(myConfig); // 설정을 기반으로 ProducerFactory 생성
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> myConfig = new HashMap<>(); // Kafka Producer 설정을 위한 Map 생성
+        myConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // Kafka 서버 설정
+        myConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // 키 역직렬화 설정
+        myConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // 값 역직렬화 설정
+
+        return new DefaultKafkaConsumerFactory<>(myConfig); // 설정을 기반으로 ConsumerFactory 생성
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> myFactory = new ConcurrentKafkaListenerContainerFactory<>(); // 리스너 컨테이너 팩토리 생성
+        myFactory.setConsumerFactory(consumerFactory()); // 생성한 ConsumerFactory를 설정
+        return myFactory; // 리스너 컨테이너 팩토리 반환
     }
 }
