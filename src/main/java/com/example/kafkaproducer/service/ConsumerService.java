a0f40d67052b46e3910df6090622c3b18afa9566
seeeop2 <seeeop2@gmail.com>
2025-01-24 21:56:02 +0900

    REFACTOR: ConsumerService의 Kafka 리스너 메소드 주석 처리
    - @KafkaListener 어노테이션을 사용한 consumer 메소드를 주석 처리하여 비활성화

--- src/main/java/com/example/kafkaproducer/service/ConsumerService.java
+++ src/main/java/com/example/kafkaproducer/service/ConsumerService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConsumerService {
 
-    @KafkaListener(topics = "fastcampus", groupId = "spring") // "fastcampus" 토픽과 "spring" 그룹 ID에 대한 리스너 설정
-    public void consumer (String message) {
-        System.out.println(String.format("Subscribed: %s", message)); // 수신한 메시지를 콘솔에 출력
-    }
+//    @KafkaListener(topics = "fastcampus", groupId = "spring") // "fastcampus" 토픽과 "spring" 그룹 ID에 대한 리스너 설정
+//    public void consumer (String message) {
+//        System.out.println(String.format("Subscribed: %s", message)); // 수신한 메시지를 콘솔에 출력
+//    }
 }
